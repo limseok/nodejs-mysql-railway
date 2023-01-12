ab57e2bbcfb8fc723f23de6ec1641aca518a5e7a
limseok <limhoseok123@gmail.com>
2023-01-12 14:22:40 -0400

    second commit

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -4,7 +4,7 @@ import db from "../database/db.js";
 //importamos sequelize
 import { DataTypes } from "sequelize";
 
-const BlogModel = db.define('blogss', {
+const BlogModel = db.define('blogsses', {
     title: { type: DataTypes.STRING },
     content: { type: DataTypes.STRING },
 });
